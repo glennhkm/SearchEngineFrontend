bbe0abe9fe021493e0c16d53838545ddd3378881
glennhkm <glenn@mhs.usk.ac.id>
2024-11-13 16:21:22 +0700

    feat: favicon web logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,11 @@ import { Footer } from "@/components/footer/footer";
 export const metadata: Metadata = {
   title: "Search Engine App",
   description: "Search Engine Kelompok 9",
+  icons: {
+    icon: ['/favicon.ico?v=4'],
+    apple: ['/apple-touch-icon.png?v=4'],
+    shortcut: ['apple-touch-icon.png']
+  }
 };
 
 export default function RootLayout({
